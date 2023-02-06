016eda008d4ae0ae9f118a7247871c76038e0fa4
David Nguyen <dnguyen@voltsystems.com>
2023-02-05 22:02:52 -0600

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,7 @@ export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
-        Copyright © {year} {config.author}. All rights reserved.
+        Copyright © {year} {config.author}
       </div>
 
       <div className={styles.settings}>
